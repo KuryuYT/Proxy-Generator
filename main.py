5bb6a57f56d7c3c7631e273d4d96992dffe25065
Agung S.T <132628875+KuryuYT@users.noreply.github.com>
2023-05-12 08:06:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import random
-
 import time 
 
 def gen_proxy():
-
   f = open('./result/proxy.txt', 'a+')
-
   asking = str(input('How Many Proxy To Scrape: '))
-
   asks = 1
-
 while asks <= asking:
-
   proxy = str(random.randint(10, 152)) + '.' + str(random.randint(10, 152)) + '.' + str(random.randint(10, 152)) + '.' + str(random.randint(10, 152))
-
   port = random.randint(22, 8080)
-
-  
-
   f.write(f'\n{proxy}:{port})
-
   f.close()
-
   asks += 1
-
-
 
 gen_proxy()
